570a51990518ddf81dea3950654bd951d4abea6d
xelloPark <yukihiro81@naver.com>
2021-08-24 22:42:05 +0900

    start 1st source

---
+++ 1.ProJect/blog/src/App.js
@@ -0,0 +1,28 @@
+import React, {useState}  from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  //let [글제목, 글제목변경] = useState('남자 코트 추천');
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집']);
+  let [글제목2, 글제목변경2] = useState('남자 코트 추천');
+
+  let posts = '강남 고기 맛집';
+  let posts2 = { color : 'white', fontSize : '30px'}
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div style = { posts2 }>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3>{ 글제목 [0]}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
