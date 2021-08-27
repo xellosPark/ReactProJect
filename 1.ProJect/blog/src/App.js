e329ba5a4c41a5f082188c6f9f5c461781d4f670
yuki <yukihiro@ubisam.com>
2021-08-27 13:04:25 +0900

    TEST

--- 1.ProJect/blog/src/App.js
+++ 1.ProJect/blog/src/App.js
@@ -1,3 +1,4 @@
+
 import React, {useState}  from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -5,7 +6,7 @@ import './App.css';
 function App() {
 
   //let [글제목, 글제목변경] = useState('남자 코트 추천');
-  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
   let [글제목2, 글제목변경2] = useState('남자 코트 추천');
 
   let posts = '강남 고기 맛집';
@@ -19,6 +20,16 @@ function App() {
       <div className="list">
         <h3>{ 글제목 [0]}</h3>
         <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목 [1]}</h3>
+        <p>4월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목 [2]}</h3>
+        <p>6월 17일 발행</p>
         <hr/>
       </div>
     </div>
