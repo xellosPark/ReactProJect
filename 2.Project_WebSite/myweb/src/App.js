4a465debe6b64c5e008eb562130d7db15cca350c
yuki <yukihiro@ubisam.com>
2021-12-05 17:02:31 +0900

    Two Porject Frist

---
+++ 2.Project_WebSite/myweb/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button, Divider, Progress} from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div>
+      <p> 안녕하세요 </p>
+      <Button primary> 방가방가 </Button>
+      <Progress percent = {66} />
+    </div>
+  
+  );
+}
+
+export default App;/
