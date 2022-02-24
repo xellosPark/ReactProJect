9649fc31d62cbbb630a2f15883dfcc1c87a150cc
yuki <yukihiro@ubisam.com>
2022-02-24 23:27:44 +0900

    first project

---
+++ 3.project/voca2/src/App.js
@@ -0,0 +1,14 @@
+import DayList from './component/DayList';
+import Header from './component/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <DayList />
+    </div>
+  );
+}
+
+export default App;
