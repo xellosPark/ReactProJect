49b0bc8fdcab5c7b962f97ecda8f7f8e71aef347
yuki <yukihiro@ubisam.com>
2022-03-01 23:37:43 +0900

    Custom Hooks add

--- 3.project/voca2/src/component/Day.js
+++ 3.project/voca2/src/component/Day.js
@@ -1,5 +1,7 @@
-import dummy from "..//db/data.json";
+// import dummy from "..//db/data.json";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 import Word from "./Word";
 
 export default function Day(){
@@ -7,20 +9,34 @@ export default function Day(){
     const { day } = useParams();
     // const day = useParams().day;
     // const day = a.day;
+    // const [words, setWords] = useState([]);
 
     // 문자와 숫자를 비교 word.day 숫자
     //const wordList = dummy.words.filter(word => word.day === day);
-    const wordList = dummy.words.filter(word => word.day === Number(day));
+    // const wordList = dummy.words.filter(word => word.day === Number(day));
     
     // const a = useParams();
     // console.log(a);
+    // useEffect(() => {
+    //     fetch(`http://localhost:3001/words?day=${day}`)
+    //         .then(res => { 
+    //             return res.json();
+    //         })
+    //         .then(data => {
+    //             setWords(data);
+    //         });
+    // }, [day]);
+
+    const words = useFetch(`http://localhost:3001/words?day=${day}`);
+
     return(
         <>
             <h2>Day {day}</h2>
             <table>
                 <tbody>
                     {/* {dummy.words.map((word) => ( */}
-                    {wordList.map((word) => (
+                    {/* {wordList.map((word) => ( */}
+                    {words.map((word) => (
                        <Word word={word} key={word.id} />
                     ))}
                 </tbody>
--- 3.project/voca2/src/component/DayList.js
+++ 3.project/voca2/src/component/DayList.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 //import dummy from "../db/data.json"; // json 객체를 dummy 라는 변수에 넣어 임포트해준다.
 
-export default function DayList(){
-//    console.log(dummy);
-    const [days, setDays] = useState([]);
-    const [count, setCount] = useState(0);
+export default function DayList() {
+//    console.log(dummy); 
+//    const [days, setDays] = useState([]);
+//  const [count, setCount] = useState(0);
 
     // function onClick(){
     //     setCount(count + 1);
@@ -22,22 +23,28 @@ export default function DayList(){
     // }
     
     //유저이펙트
-    useEffect(() => {
-        //console.log("Count change");
-        // fetch 비동기
-        fetch('http://localhost:3001/days')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setDays(data);
-            });
-    },[]); 
-    //useEffect 를 사용 할 때에는 첫번째 파라미터에는 함수,
+    const days = useFetch(`http://localhost:3001/days`);
+    // useEffect(() => {
+    //     //console.log("Count change");
+    //     // fetch 비동기
+    //     // const fetchResponsePromise = fetch(resource [, init])
+    //     fetch(`http://localhost:3001/days`)
+    //     //.then (promise)프라미스
+    //     //Return value
+    //     //A Promise that resolves to a Response object.
+    //         .then(res => { 
+    //             return res.json();
+    //         })
+    //         .then(data => {
+    //             setDays(data);
+    //         });
+    // },[]); 
+    // useEffect 를 사용 할 때에는 첫번째 파라미터에는 함수,
     // 두번째 파라미터에는 의존값이 들어있는 배열 (deps)을 넣습니다.
     // 만약에 deps 배열을 비우게 된다면, 컴포넌트가 처음 나타날때에만
     // useEffect 에 등록한 함수가 호출됩니다.
-    // 그리고, useEffect 에서는 함수를 반환 할 수 있는데 이를 cleanup 함수라고 부릅니다. cleanup 함수는 useEffect 에 대한 뒷정리를 해준다고 이해하시면 되는데요, deps 가 비어있는 경우에는 컴포넌트가 사라질 때 cleanup 함수가 호출됩니다.
+    // 그리고, useEffect 에서는 함수를 반환 할 수 있는데 이를 cleanup 함수라고 부릅니다. cleanup 함수는 useEffect 에 대한 뒷정리를 해준다고 이해하시면 되는데요,
+    // deps 가 비어있는 경우에는 컴포넌트가 사라질 때 cleanup 함수가 호출됩니다.
     // [count] deps 에 특정 값 넣기
     // deps 에 특정 값을 넣게 된다면, 컴포넌트가 처음 마운트 될 때에도 호출이 되고,
     // 지정한 값이 바뀔 때에도 호출이 됩니다. 그리고, deps 안에 특정 값이 있다면
