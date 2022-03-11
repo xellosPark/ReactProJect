e33e8e2b37fd1ea7e3c46c3a79ef27d4b83b5c80
yuki <yukihiro@ubisam.com>
2022-03-11 23:02:13 +0900

    first source

--- 3.project/loinemui/src/App.js
+++ 3.project/loinemui/src/App.js
@@ -1,24 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { Avatar, Box, Button, Checkbox, Container, FormControlLabel, Grid, Link, TextField, Typography } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App" component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        {/* 아이콘 원으로 및 색상 */}
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          {/* icon 사용 */}
+          <LockOutlinedIcon />
+        </Avatar>
+        {/* h1 스타일 h5 */}
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        {/* required 반드시 삽입 */}
+        <TextField 
+          margin="normal"
+          label="Email Address" 
+          required name="email" 
+          autoComplete="email" 
+          autoFocus 
+          fullWidth
+        />
+        <TextField
+          margin="normal"
+          label="Password"
+          type="password"
+          required
+          fullWidth
+          name="password"
+          autoComplete="current-password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2}} fullWidth> Sign in </Button>
+        {/*Grid container 행 */}
+        {/* xs auto laye */}
+        <Grid container>
+          <Grid item xs>
+            <Link href="#" variant="body2">
+              Forgot password?
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link href="#" variant="body2">
+              {"Don't have an account? Sign Up"}
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 }
 
