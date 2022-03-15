d1e53307eb5f989cbc2aeb0606e21e2ae414c148
ParkDongJun <yuki@ubisam.com>
2022-03-15 17:05:02 +0900

    React APi frest add

---
+++ 4.React_api/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const server = express();
+const bodyParser = require('body-parser');
+
+server.use(bodyParser.json());
+
+const users = [
+    {
+        id: "1",
+        name: "홍기동",
+        email: "bbb@com"
+    },
+    {
+        id: "2",
+        name: "유재석",
+        email: "aaa@com"
+    }
+];
+
+server.get("/api/user",(req,res) => {
+    
+    res.json(users);
+});
+
+server.post("/api/user",(req,res) => {
+    // console.log(req.body);
+    users.push(req.body);
+    res.json(users);
+});
+
+
+
+server.listen(3000, () => {
+    console.log("The Server is running");
+});
